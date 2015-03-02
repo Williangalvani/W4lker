a1f12646bc388a5de08935b192f8160ed2e9926f
Willian Galvani <williangalvani@gmail.com>
2015-03-02 02:54:25 +0200

    absence of realrobot no longer stops code execution

--- legs.py
+++ legs.py
@@ -4,16 +4,23 @@ from robot.robotInterfaces.virtualRobot.virtualRobot import VirtualRobot
 from robot.robotInterfaces.realRobot.realRobot import RealRobot
 
 robot1 = VirtualRobot()
-robot2 = RealRobot()
+try:
+    robot2 = RealRobot()
+except:
+    robot2 = None
 controller1 = controller.RobotController(robot1)
 controller1.start()
 
-controller2 = controller.RobotController(robot2)
-controller2.start()
+controller2 = None
+if robot2:
+    controller2 = controller.RobotController(robot2)
+    controller2.start()
 
 print("script ready!")
 
 def run():
     global controller1, controller2
     controller1.iterate()
-    controller2.iterate()
+
+    if robot2:
+        controller2.iterate()
